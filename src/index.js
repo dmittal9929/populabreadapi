dd0ef8218e137585eb92dcef5f550e98ec38fd0c
dmittal9929 <mittaldhruv29@gmail.com>
2020-05-13 21:08:58 +0530

    minor fixes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT ;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 app.use(productRouter);
 app.use(specialsRouter);
 app.use(adminRouter);
@@ -30,7 +30,8 @@ app.use(collabUpdateRouter);
 
 app.listen(PORT,()=>{
     console.log("listening to port " + PORT);
-})
+});
+
 
 
 
